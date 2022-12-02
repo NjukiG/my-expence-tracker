3f68f8439677a71ec25e74711db6aac4f3636e48
George <gnjuki19@gmail.com>
2022-12-02 14:00:42 +0300

    Got the balances from mapping and reducing the amounts

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 function Balance() {
+  const {transactions} = useContext(GlobalContext)
+
+  const amounts = transactions.map((transaction) => transaction.amount)
+
+  const totalAmount = amounts.reduce((amount, index) => (amount += index), 0).toFixed(2)
+  console.log(totalAmount)
   return (
     <div>
       <h4>Your Balance</h4>
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,10 +2,11 @@ import React from "react";
 
 function Transaction({transaction}) {
     const {text, amount} = transaction
+    const sign = amount < 0 ? "-" : "+"
   return (
     <div>
-      <li className="minus">
-        {text} <span>{amount}</span>
+      <li className={amount < 0 ? "minus" : "plus"}>
+        {text} <span>{sign} ${Math.abs(amount)}</span>
         <button className="delete-btn">x</button>
       </li>
     </div>
